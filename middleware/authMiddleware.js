473cb9c5ec1d1ddaa48cfd9257747229646795f0
dakshbhardwaj <daksh@truesparrow.com>
2025-02-25 12:51:25 +0530

    updated middleware to handle token coming with Bearer as prefix

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,14 +1,15 @@
 const jwt = require("jsonwebtoken");
 
 module.exports = (req, res, next) => {
-    const token = req.header("Authorization");
-    if (!token) return res.status(401).json({ error: "Access denied" });
+    const authHeader = req.header("Authorization");
+    if (!authHeader) return res.status(401).json({ error: "Access denied" });
 
     try {
+        const token = authHeader.replace('Bearer ', '');
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
         req.user = decoded;
         next();
-    } catch {
-        res.status(400).json({ error: "Invalid token" });
+    } catch (error) {
+        res.status(401).json({ error: "Invalid token" });
     }
 };
